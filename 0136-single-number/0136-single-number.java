813b838ec9d8f749be00c3b5e416e49e50e0a7cd
chinmayeenb25 <257099448+chinmayeenb25@users.noreply.github.com>
2026-08-25 06:55:40 +0530

    Time: 1 ms (99.94%), Space: 46.6 MB (92.74%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int output=0;
+        for(int i=0;i<nums.length;i++){
+            output^=nums[i];
+        }
+        return output;
+        
+    }
+}
